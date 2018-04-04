e88fbd90a58423d39024c8335367ec4a8afa31f9
Salvian Reynaldi <5778914+salvianreynaldi@users.noreply.github.com>
2018-04-04 14:37:42 +0700

    Hotfix random ID length and health check config (#3)
    * reduce random_id length and use the hex output,
    
    * fix typo

--- locals.tf
+++ locals.tf
@@ -7,17 +7,17 @@ resource "random_id" "lb_suffix" {
     tg_vpc_id          = "${var.vpc_id}"
   }
 
-  byte_length = 8
+  byte_length = 4
 }
 
 locals {
-  lb_name           = "${var.lb_name == "" ? format("%s-%s-%s", var.service_name, var.lb_internal ? "lbint" : "lbext", random_id.lb_suffix.b64_url) : var.lb_name}"
+  lb_name           = "${var.lb_name == "" ? format("%s-%s-%s", var.service_name, var.lb_internal ? "lbint" : "lbext", random_id.lb_suffix.hex) : var.lb_name}"
   tg_name           = "${var.tg_name == "" ? format("%s-%s", local.lb_name, "default") : var.tg_name}"
   target_group_arns = "${concat(list(aws_lb_target_group.default.arn), var.target_group_arns)}"
 }
 
 locals {
-  tg_health_check = {
+  tg_default_health_check = {
     "interval"            = 30
     "path"                = "/healthcheck"
     "port"                = 5000
@@ -27,4 +27,6 @@ locals {
     "protocol"            = "HTTP"
     "matcher"             = "200"
   }
+
+  tg_health_check = "${merge(local.tg_default_health_check, var.tg_health_check)}"
 }
