2075435d0bb60207b9541974d9ed906fd3cbb915
vincenttjia <48272053+vincenttjia@users.noreply.github.com>
2023-05-12 13:35:54 +0700

    Updated the ssl_policy to ELBSecurityPolicy-TLS13-1-2-2021-06

--- variables.tf
+++ variables.tf
@@ -127,7 +127,7 @@ variable "listener_certificate_arn" {
 
 variable "listener_ssl_policy" {
   type        = string
-  default     = "ELBSecurityPolicy-2016-08"
+  default     = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   description = "The LB listener's SSL policy"
 }
 
